f3525a0fe12a0771aa6d0841b21289533bb67b5a
NKayser <nico.a.kayser@gmail.com>
2022-11-07 14:10:38 +0100

    Find core references with invalid number of references

--- MyCode/spacy_causal_carbon/scripts/make_corpus.py
+++ MyCode/spacy_causal_carbon/scripts/make_corpus.py
@@ -4,6 +4,21 @@ from spacy.tokens import DocBin
 import json
 
 
+def get_ent_with_id(ents, id):
+    for ent in ents:
+        if str(ent["id"]) == str(id):
+            return ent
+    return None
+
+
+def get_relations_from_ent(rels, ent_id):
+    return list(filter(lambda rel: str(rel["from_id"]) == str(ent_id), rels))
+
+
+def get_to_ents_from_relations(ents, rels):
+    return list(map(lambda rel: get_ent_with_id(ents, rel["to_id"]), rels))
+
+
 def create_docbin(fname: str, basename: str, nlp):
     with open(fname, 'r') as json_file:
         json_list = list(json_file)
@@ -13,18 +28,25 @@ def create_docbin(fname: str, basename: str, nlp):
     for json_str in tqdm(json_list):
         result = json.loads(json_str)
         doc = nlp.make_doc(result["text"])  # create doc object from text
-        ents = []
-        for ent in result["entities"]:  # add character indexes
+        doc_ents = []
+        entities = result["entities"]
+        relations = result["relations"]
+        for ent in entities:  # add character indexes
             start = ent["start_offset"]
             end = ent["end_offset"]
             label = ent["label"]
+            id = ent["id"]
             span = doc.char_span(start, end, label=label, alignment_mode="contract")
-            if span is None or len(nlp(span.text)) > 6 or label != "timeline":
+            if span is None:# or len(nlp(span.text)) > 6 or label != "emissions":
                 #print("Skipping entity")
                 continue
             else:
-                ents.append(span)
-        doc.spans["sc"] = ents  # span groups: might modify later to make core references one group
+                if label == "core reference":
+                    relations = get_relations_from_ent(relations, id)
+                    to_ents = get_to_ents_from_relations(entities, relations)
+                    print(str(len(relations)) + ", " + str(to_ents) + ": " + span.text)
+                doc_ents.append(span)
+        doc.spans["sc"] = doc_ents  # span groups: might modify later to make core references one group
         db.add(doc)
 
     db.to_disk(f"./corpus/{basename}.spacy")  # save the docbin object
