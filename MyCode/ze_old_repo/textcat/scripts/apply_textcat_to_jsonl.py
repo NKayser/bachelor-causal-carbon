7bde9d0487b1df2408d924546c128de1b5399967
Nico Kayser <nico.a.kayser@gmail.com>
2022-12-08 15:38:40 +0100

    Remove cval2 predictions with cval3 predictions

--- MyCode/ze_old_repo/textcat/scripts/apply_textcat_to_jsonl.py
+++ MyCode/ze_old_repo/textcat/scripts/apply_textcat_to_jsonl.py
@@ -2,17 +2,18 @@ import spacy
 from tqdm import tqdm
 import json
 
+from MyCode.scripts.consts import INPUT_PATH
+from MyCode.scripts.utils import read_input_file
+
 # load the best model from training
-nlp = spacy.load("../../../textcat/models/cval_2/model-best")
+nlp = spacy.load("../textcat/models/cval_3/model-best")
 
 spacy.prefer_gpu(0)
 
-with open("../../spancat/results/textcat_all_with_pretrained_ner_output_2.jsonl", 'r', encoding='utf-8') as json_file:
-    json_list = list(json_file)
+json_list = read_input_file()
 
-with open("../results/textcat_all_with_ner_and_predictions.jsonl", 'w', encoding='utf-8') as out_file:
-    for json_str in tqdm(json_list):
-        result = json.loads(json_str)
-        doc = nlp(result["text"])
-        result["textcat_prediction"] = doc.cats["positive"]
-        out_file.write(json.dumps(result) + "\n")
+with open(INPUT_PATH + "2", 'w', encoding='utf-8') as out_file:
+    for obj in tqdm(json_list):
+        doc = nlp(obj["text"])
+        obj["textcat_prediction"] = doc.cats["positive"]
+        out_file.write(json.dumps(obj) + "\n")
