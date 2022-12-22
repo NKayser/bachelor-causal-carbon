a02d9247b36d0bd945b92883a1f8dbd6549d8c67
NKayser <nico.a.kayser@gmail.com>
2022-12-22 15:11:50 +0100

    More detailed evaluation

--- MyCode/entity_categorization/scripts/evaluate.py
+++ MyCode/entity_categorization/scripts/evaluate.py
@@ -6,6 +6,7 @@ from spacy.tokens import DocBin
 from tqdm import tqdm
 
 from MyCode.entity_categorization.scripts.custom_span_suggester import build_custom_suggester
+from MyCode.scripts.process_article import Article
 from MyCode.scripts.utils import write_json_to_file
 
 
@@ -20,13 +21,14 @@ db_test = DocBin().from_disk("corpus-unbalanced/test.spacy")
 #db_all.merge(db_dev)
 #db_all.merge(db_test)
 nlp = spacy.load("models-binary/model-best")
+nlp2 = spacy.load("en_core_web_trf")
 
 labels = ["DATE", "FAC", "GPE", "PRODUCT", "MONEY", "PERCENT", "QUANTITY"]
-#class_labels = []
-#for label in labels:
-#    class_labels.append(label + " positive")
-#    class_labels.append(label + " negative")
-class_labels = ["positive", "negative"]
+class_labels = []
+for label in labels:
+    class_labels.append(label + " positive")
+    class_labels.append(label + " negative")
+#class_labels = ["positive", "negative"]
 
 confusion_matrix = {actual_class: {predicted_class: 0
                                    for predicted_class in class_labels}
@@ -40,18 +42,24 @@ for doc in tqdm(list(db_test.get_docs(nlp.vocab))):
     except ValueError:
         print("Error with loading doc")
         continue
+    article = Article(doc.text)
+    article.set_money_ents()
+    ner_ents = article.doc.ents
     #print([[span.start_char, span.end_char, span.text, span.label_] for span in predicted_doc.spans["sc"]])
     for predicted_span in predicted_doc.spans["sc"]:
         for true_span in doc.spans["sc"]:
             if predicted_span.start_char == true_span.start_char and predicted_span.end_char == true_span.end_char:
-                found_predicted = True
-                #true_label = true_span.label_.split()
-                #predicted_label = predicted_span.split()
-                confusion_matrix[true_span.label_][predicted_span.label_] += 1
+                for ent in ner_ents:
+                    if ent.start_char == true_span.start_char and ent.end_char == true_span.end_char:
+                        found_predicted = True
+                        #true_label = true_span.label_.split()
+                        #predicted_label = predicted_span.split()
+                        confusion_matrix[ent.label_ + " " + true_span.label_][predicted_span.label_] += 1
+                        break
                 break
 
 
 filtered_confusion_matrix = {k: {k2: v2 for k2, v2 in v.items() if v2 > 0} for k, v in confusion_matrix.items()}
 print(json.dumps(filtered_confusion_matrix, indent=4))
 
-write_json_to_file(filtered_confusion_matrix, "metrics/binary_confusion_matrix.json")
+write_json_to_file(filtered_confusion_matrix, "metrics/binary_confusion_matrix.jsonl")
